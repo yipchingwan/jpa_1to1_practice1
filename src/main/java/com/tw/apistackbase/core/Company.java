01b432addd62574977a248dc134701190397c59d
KYLE\Surface <kyle.public10415@gmail.com>
2018-11-29 16:02:19 +0800

    added companyProfile.java

--- src/main/java/com/tw/apistackbase/core/Company.java
+++ src/main/java/com/tw/apistackbase/core/Company.java
@@ -1,9 +1,6 @@
 package com.tw.apistackbase.core;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Company {
@@ -29,10 +26,22 @@ public class Company {
 
     private String name;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    private CompanyProfile companyProfile;
+
     public Company() {
     }
 
-    public Company(String name) {
+    public Company(String name, CompanyProfile companyProfile) {
         this.name = name;
+        this.companyProfile = companyProfile;
+    }
+
+    public CompanyProfile getCompanyProfile() {
+        return companyProfile;
+    }
+
+    public void setCompanyProfile(CompanyProfile companyProfile) {
+        this.companyProfile = companyProfile;
     }
 }
